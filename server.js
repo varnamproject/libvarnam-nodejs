442c1192bd8c87e8ba244b8993f599def0b20909
Jijesh Mohan <jijeshmohan@gmail.com>
2012-08-19 13:17:03 +0530

    modified server.js with varnam class

--- server.js
+++ server.js
@@ -4,20 +4,22 @@ var file="ml-unicode.vst";
 
 var url = require('url');
 
+var file = "ml-unicode.vst";
+
+var varnam = new addon.Varnam(file);
+
 http.createServer(function(req,res) {
     var input = url.parse(req.url,true).query.input;
-    if(input === undefined){
-       res.writeHead(404);
-       res.write('Bad request 404\n');
-       res.end(); 
-    }else{
-        addon.runCallback(file,input,function(msg){
+        if(input === undefined){
+           res.writeHead(404);
+           res.write('Bad request 404\n');
+           res.end();
+        }else{
+            var msg = varnam.transliterate(input);
              res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8;', 'Access-Control-Allow-Origin': '*'});
              res.write(msg);
             res.end();
-        });
-       
-    }
-}).listen(3003);
+          }
+    }).listen(3003);
 
 console.log("Server is running in 3003");
--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var v = require('./build/Release/lib.target/varnam');
 var file = "ml-unicode.vst";
 
 var varnam = new v.Varnam(file)
-str = varnam.transliterate("mOhan");
+str = varnam.transliterate("#mOhan");
 console.log(str);
 
 str = varnam.reverseTransliterate(str);
